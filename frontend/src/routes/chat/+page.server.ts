2b92ed743cd60d6e2fa95dc14361e766ed6875ad
Daniel Tait <tait.djk@gmail.com>
2024-01-18 18:50:50 +0000

    needed text on message!

--- frontend/src/routes/chat/+page.server.ts
+++ frontend/src/routes/chat/+page.server.ts
@@ -1,6 +1,17 @@
+import prisma from "$lib/prisma";
 import type { PageServerLoad } from "./$types";
 
 export const load = (async( ) => 
 {
-
+    const chatSession = await prisma.chatSession.findUniqueOrThrow({
+        where: {
+            id: 1
+        },
+        include: {
+            messages: true
+        }
+    });
+    return {
+        chat: chatSession
+    }
 }) satisfies PageServerLoad
--- frontend/src/routes/chat/+page.svelte
+++ frontend/src/routes/chat/+page.svelte
@@ -1,3 +1,12 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+    export let data: PageData
+</script>
+<div>
+    {#each data.chat.messages as msg}
+    {msg.text}
+    {/each}
+</div>
 <h1>Chat</h1>
 <form method="POST" action="?/sendmsg">
     <textarea name="propmt"></textarea>
